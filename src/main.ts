70ed8388a6ec86cc6a3c4c74171dc8cd6e07b613
JimanPark <jiman.park@sk.com>
2017-11-15 09:34:34 +0900

    [FEATURE] parsing HTML & response by puppeteer, Express

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ const app = express();
 
 app.enable('trust proxy');
 
-
 app.get('/', (req, res) => {
   const parser = new Parser();
-  parser.excute();
-  res.send('Puppeteer');
+  parser.excute()
+    .then((result) => {
+      res.send(result);
+    })
 })
 
 app.listen(4200, () => {
--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,30 +1,23 @@
 import * as puppeteer from 'puppeteer';
-import * as process from 'process';
-import * as path from 'path';
-
 
 export class Parser {
-  private BASE_DIR = process.cwd();
+  public async excute() {
+    let browser = await puppeteer.launch();
+    let page = await browser.newPage();
+    await page.goto('http://m.ruliweb.com');
 
-  public excute() {
-    (async () => {
-      let browser = await puppeteer.launch();
-      let page = await browser.newPage();
-      await page.goto('http://m.ruliweb.com', { waitUntil: 'networkidle2' });
-
-      const links = await page.evaluate(() => {
-        const anchors = Array.from(document.querySelectorAll('#m_hit_article_2 .widget_bottom .subject.deco'));
-        return anchors.map(anchor => {
-          return {
-            link: anchor.href,
-            text: anchor.innerText
-          }
-        });
+    const links = await page.evaluate(() => {
+      const anchors = Array.from(document.querySelectorAll('#m_hit_article_2 .widget_bottom .subject.deco'));
+      return anchors.map((anchor: HTMLAnchorElement) => {
+        return {
+          link: anchor.href,
+          text: anchor.innerText
+        }
       });
+    });
 
-      // await page.screenshot({ path: path.join(process.cwd(), 'generated', 'test.png') });
-      await browser.close();
-    })();
+    await browser.close();
+    return links;
   }
 }
 
